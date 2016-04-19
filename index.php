00657b2d73ef4613439552312ef6de3e4282cb4e
sergeimaslov86 <sergei.maslov86@gmail.com>
2016-04-19 10:46:28 +0300

    seo fixes

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ if (!isset($sections)) {
 <div class="page-wrap">
     <div class="rd-mobilemenu active">
         <div class="logo">
-            <a href="<?php echo $path; ?>">
+            <a href="<?php echo $path; ?>/">
                 <img src="<?php echo $path; ?>/img/logo.png" alt="">
             </a>
         </div>
